7350b1b4f7a0a98c6db14ae8939d64049522e13b
colderry <c0l4e6r7@gmail.com>
2022-07-31 20:33:55 +0600

    feat: add new props to user

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,31 +1,67 @@
 import mongoose, { Schema, model, Document } from "mongoose";
 
 const UserSchema = new Schema({
-	_id: String,
-	username: {
-		type: String,
-		required: true,
-		unique: true
+	_id: Number,
+
+	api: {
+		key: {
+			type: String,
+			required: true
+		}
 	},
+
+	avatar: {
+		type: String,
+		default: null
+	},
+
 	email: {
 		type: String,
 		required: true,
 		unique: true
 	},
-	avatar: {
-		
+
+	password: {
+		type: String,
+		default: null
+	},
+
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+
+	signup_type: {
+		type: String,
+		required: true
+	},
+
+	verified: {
+		type: Boolean,
+		default: false
 	}
+
 }, {
 	versionKey: false,
 	timestamps: true
 });
 
 export interface UserDocument extends Document {
-	_id: string;
-	username: string;
+	_id: number;
+	api: UserDocumentApi;
+	avatar: string | null;
 	email: string;
+	password: string | null;
+	username: string;
+	signup_type: string;
+	verified: boolean;
 	createdAt: string;
 	updatedAt: string;
+}
+
+export interface UserDocumentApi {
+	key: string
 }
 
 export default mongoose.models.users || model<UserDocument>("users", UserSchema);
